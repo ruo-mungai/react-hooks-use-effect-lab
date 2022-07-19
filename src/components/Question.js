75a55269a7e365862d1829ef92672e4f576250ae
Patrick <ruomungai@gmail.com>
2022-07-19 17:23:53 +0300

    commit tree 2f01e5468694a8597de5849cbedc91aa06da7174

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect,useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(()=>{
+    const timer = setTimeout(() => {
+      setTimeRemaining(timeRemaining-1);
+
+       if (timeRemaining === 0) {
+        setTimeRemaining(timeRemaining+10);
+        onAnswered(false)
+      }
+    }, 1000);
+
+      return function cleanup() {
+      clearTimeout(timer);
+    };
+
+  })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
